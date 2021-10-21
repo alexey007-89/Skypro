8551e926b0c1d60ba5c65eef2ecf26621ac111e8
Aleksey Pakhomov <Alexey007-89@mail.ru>
2021-10-21 12:20:30 +1000

    Задание 6

--- src/ru/skypro/homework4/Main.java
+++ src/ru/skypro/homework4/Main.java
@@ -1,4 +1,4 @@
-package ru.skypro;
+package ru.skypro.homework4;
 
 public class Main {
 
---
+++ src/ru/skypro/homework6/hw6.java
@@ -0,0 +1,7 @@
+package ru.skypro.homework6;
+
+public class hw6 {
+    public static void main(String[] args) {
+
+    }
+}
